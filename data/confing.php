0f2b8fbc88f8da50f8ff246edf2cb08e23140d9f
source-trace <2456906317@qq.com>
2017-08-06 20:08:10 -0700

    V4.0

---
+++ data/confing.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD','');
+define('DB_NAME', 'beescms_x2');
+define('DB_PRE', 'bees_');
+define('DB_PCONNECT', 0);
+define('DB_CHARSET', 'utf8');
+define('CMS_ADDTIME', 1436276598);
+define('CMS_SELF','/beescms_x2/');
+?>
